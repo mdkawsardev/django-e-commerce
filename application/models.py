78afff39643a1bc988edacd471f499387e8e67c1
mdkawsar <info.mdkawsardev@gmail.com>
2026-04-06 21:21:56 +0600

    Git repository added

---
+++ application/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from base.models import BaseModel
+
+class Category(BaseModel):
+    category_name = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True, null=True, blank=True)
+    category_image = models.ImageField(upload_to='category_image')
+
+class Product(BaseModel):
+    product_name = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True, null=True, blank=True)
+    product_name = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
+    price = models.IntegerField()
+    description = models.TextField()
+
+class ProductImage(BaseModel):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='product_image')
+    image = models.ImageField(upload_to='product')
