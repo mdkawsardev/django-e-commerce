8fbf6c38f3f7d3dbfeb9b5ab6d5254e06da7303a
mdkawsar <info.mdkawsardev@gmail.com>
2026-04-08 11:58:17 +0600

    New code added

--- application/admin.py
+++ application/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['category_name', 'slug', 'category_image']
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['product_name', 'slug', 'category', 'price']
+@admin.register(ProductImage)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['product', 'image']
--- application/models.py
+++ application/models.py
@@ -4,15 +4,19 @@ from base.models import BaseModel
 class Category(BaseModel):
     category_name = models.CharField(max_length=100)
     slug = models.SlugField(unique=True, null=True, blank=True)
-    category_image = models.ImageField(upload_to='category_image')
+    category_image = models.ImageField(upload_to='media/category_image')
+    def __str__(self):
+        return self.category_name
 
 class Product(BaseModel):
     product_name = models.CharField(max_length=100)
     slug = models.SlugField(unique=True, null=True, blank=True)
-    product_name = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True, blank=True, related_name='products')
     price = models.IntegerField()
     description = models.TextField()
+    def __str__(self):
+        return self.product_name
 
 class ProductImage(BaseModel):
     product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='product_image')
-    image = models.ImageField(upload_to='product')
+    image = models.ImageField(upload_to='media/product')
